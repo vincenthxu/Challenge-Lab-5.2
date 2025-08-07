aff798f794abbd5640c76b0125601a85dddd5736
Vincent Xu <vincent.xu1@outlook.com>
2025-08-07 14:52:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Challenge_Lab_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = { 1, 2, 3, 4, 5, 6, 7 };
+            Console.WriteLine(FindMissingElement(elements));
+
+            elements = [3, 0, 1];
+            Console.WriteLine(FindMissingElement(elements));
+
+            elements = [0, 1];
+            Console.WriteLine(FindMissingElement(elements));
+
+            elements = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+            Console.WriteLine(FindMissingElement(elements));
+        }
+
+        static int FindMissingElement(int[] elements)
+        {
+            HashSet<int> missing = new HashSet<int>();
+
+            for (int i = 0; i <= elements.Length; i++)
+            {
+                missing.Add(i);
+            }
+
+            foreach (int element in elements)
+            {
+                missing.Remove(element);
+            }
+
+            return missing.First();
+        }
+    }
+}
